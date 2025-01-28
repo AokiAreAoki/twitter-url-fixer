9dbc78684ce337181be50b65f291ecbf607563d9
Aoki <ger.vladislav2001@gmail.com>
2025-01-29 01:11:35 +0200

    init commit

---
+++ src/main.rs
@@ -0,0 +1,82 @@
+use clipboard_master::{CallbackResult, Master};
+use clipboard_win::{get_clipboard_string, set_clipboard_string};
+use regex::{Captures, Regex};
+use std::{io, thread::sleep, time};
+
+#[macro_use]
+extern crate lazy_static;
+
+const REPLACE_DELAY: u64 = 150;
+
+fn callback() -> CallbackResult {
+    lazy_static! {
+        static ref TWITTER_RE: Regex = match Regex::new(
+            r"(?P<p>https?://)(?:\w+\.)?(?:(?:vxtwitter|twitter|x)\.com)(?P<u>/[\w+@_-]+/status/\w+)(\?\S+)?"
+        ) {
+            Ok(re) => re,
+            Err(err) => panic!("{}", err),
+        };
+        static ref DISCORD_RE: Regex = match Regex::new(
+            r"(?P<link>https?://(?:\w+\.)?discordapp\.(?:com|net)/attachments/[/\w._-]+)(\?\S+)?"
+        ) {
+            Ok(re) => re,
+            Err(err) => panic!("{}", err),
+        };
+    }
+
+    sleep(time::Duration::from_millis(REPLACE_DELAY));
+
+    match get_clipboard_string() {
+        Err(_) => {
+            return CallbackResult::Stop;
+        }
+        Ok(string) => {
+            let mut fixed_string = string.clone();
+
+            fixed_string = TWITTER_RE
+                .replace_all(fixed_string.as_str(), |cap: &Captures| {
+                    let protocol = cap.get(1).unwrap().as_str();
+                    let uri = cap.get(2).unwrap().as_str();
+
+                    format!("{}vxtwitter.com{}", protocol, uri)
+                })
+                .into_owned();
+
+            // fixed_string = DISCORD_RE
+            //     .replace_all(fixed_string.as_str(), |cap: &Captures| {
+            //         cap.get(1).unwrap().as_str().to_owned()
+            //     })
+            //     .into_owned();
+
+            if string == fixed_string {
+                return CallbackResult::Stop;
+            }
+
+            if let Err(err) = set_clipboard_string(&fixed_string) {
+                println!("Error setting clipboard: {}", err);
+                return CallbackResult::Stop;
+            }
+
+            println!("Replace:");
+            println!("- {}", string);
+            println!("+ {}", fixed_string);
+
+            CallbackResult::Next
+        }
+    }
+}
+
+fn error_callback(error: io::Error) -> CallbackResult {
+    println!("Error listening to clipboard: {}", error);
+    CallbackResult::Stop
+}
+
+fn main() {
+    println!("Listening to clipboard");
+    let delay = time::Duration::from_millis(100);
+
+    loop {
+        let _ = Master::new(callback, error_callback).run();
+        sleep(delay);
+    }
+}
